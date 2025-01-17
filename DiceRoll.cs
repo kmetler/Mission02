05c9dad5a8bb96c6422067f530602ccda2b2a969
Kaitlyn <Kaitlyn@KAITLYNHP>
2025-01-17 10:38:45 -0700

    Add project files.

---
+++ DiceRoll.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Mission02
+{
+    internal class DiceRoll
+    {
+        private Random rnd = new Random();
+
+        public void RollDice (int numRolls, int[] aTotals)
+        {
+            //for the number of times they said
+            for (int i = 0; i < numRolls; i++)
+            {
+                //generate a random number 1-6
+                int randNum1 = rnd.Next(1, 7);
+                //generate second random number 1-6
+                int randNum2 = rnd.Next(1, 7);
+
+                //Add two numbers together
+                int total = randNum1 + randNum2;
+
+                //go update counter of each number
+                aTotals[total]++;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using Mission02;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //ask user how many rolls they want
+        System.Console.WriteLine("Welcome to the dice throwing simulator!\n");
+        System.Console.WriteLine("How many dice rolls would you like to simulate?");
+        
+        int numRolls = int.Parse(System.Console.ReadLine());
+        // create array for totals of dice
+        int[] aTotals = new int[13];
+
+        //simulate rolls
+        DiceRoll diceRoll = new DiceRoll();
+        diceRoll.RollDice(numRolls, aTotals);
+        
+        //print results
+        PrintResult printResult = new PrintResult();
+        printResult.PrintOffResults(numRolls, aTotals);
+    }
+}
